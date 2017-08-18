596ae4bb64eabe169cdafb1be31dda805db876f4
Anselmo Pfeifer <apfeifer@daitangroup.com>
2017-08-18 13:10:53 -0300

    WIP

--- src/main/groovy/com/anselmopfeifer/jobs/projects/projectJobs.groovy
+++ src/main/groovy/com/anselmopfeifer/jobs/projects/projectJobs.groovy
@@ -1,2 +1,17 @@
 package com.anselmopfeifer.jobs.projects
 
+freeStyleJob('create-project-jobs') {
+    logRotator(-1, 10)
+    jdk('Java 8')
+
+    steps {
+        shell(
+                """
+ls -la
+pwd
+date
+echo "Hello Project"
+"""
+        )
+    }
+}
