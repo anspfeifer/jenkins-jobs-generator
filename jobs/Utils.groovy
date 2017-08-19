8dc13184af2b2e8ac983f6689f88c835e7696547
Anselmo Pfeifer <anspfeifer@gmail.com>
2017-08-19 00:07:32 -0300

    WIP

---
+++ jobs/Utils.groovy
@@ -0,0 +1,12 @@
+class Utils {
+
+    static defaultChefRevision = 'v0.477'
+
+    static getGenerationDate() {
+        System.getProperty("generationDate")?:new Date()
+    }
+
+    static String getJobDescription() {
+        "Gerado por <a href='http://github.com/anspfeifer/jenkins-jobs-generator'>github.com/anspfeifer/jenkins-jobs-generator</a> em ${getGenerationDate()}"
+    }
+}
---
+++ jobs/serverJobs.groovy
@@ -0,0 +1,5 @@
+import static Utils.*
+
+jobFactory.freeStyleJob('shell-script-job') {
+    description getJobDescription()
+}
