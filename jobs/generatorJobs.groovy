1f82d297a2122984a9ee2c54bd9fd2c8ee50ddc9
Anselmo Pfeifer <anspfeifer@gmail.com>
2017-08-19 19:42:04 -0300

    WIP

---
+++ jobs/generatorJobs.groovy
@@ -0,0 +1,28 @@
+import static Utils.getJobDescription
+
+job('generator-jobs') {
+    description getJobDescription()
+
+    logRotator (-1, 10)
+
+    jdk ('Java 8')
+
+    scm {
+        github 'anspfeifer/jenkins-jobs-generator'
+    }
+
+    steps {
+        dsl {
+            external 'jobs/**/*Jobs.groovy'
+            additionalClasspath 'src/main/groovy'
+        }
+    }
+
+    triggers {
+        scm 'H/5 * * * *'
+    }
+
+    publishers {
+        email 'anspfeifer@gmail.com'
+    }
+}
---
+++ jobs/servers/chefProvisionJobs.groovy
@@ -0,0 +1,31 @@
+package servers
+
+import static Utils.getJobDescription
+
+jobFactory.freeStyleJob('chef-provision-job') {
+    description getJobDescription()
+
+    logRotator (-1, 10)
+
+    jdk ('Java 8')
+
+    parameters{
+        stringParam('VERSION', 'master', 'Version chef Release')
+    }
+
+    steps {
+        shell ("""
+echo ${getJobDescription}
+echo date
+""")
+    }
+
+    triggers {
+        scm 'H/5 * * * *'
+    }
+
+    publishers {
+        email 'anspfeifer@gmail.com'
+    }
+
+}
