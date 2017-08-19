3b7a52bdf6ccf68900a8257c4a2df9bad1d741a4
anspfeifer <30416051+anspfeifer@users.noreply.github.com>
2017-08-19 00:43:23 -0300

    Update serverJobs.groovy

--- jobs/serverJobs.groovy
+++ jobs/serverJobs.groovy
@@ -1,8 +1,16 @@
-import static Utils.*
-
-jobFactory.freeStyleJob('shell-script-job') {
-    
-    logRotator (-1, 10)
-    jdk ('Java 8')
-
+freeStyleJob('example') {
+    logRotator(-1, 10)
+    jdk('Java 6')
+    scm {
+        github('jenkinsci/job-dsl-plugin', 'master')
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        gradle('clean build')
+    }
+    publishers {
+        archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
+    }
 }
