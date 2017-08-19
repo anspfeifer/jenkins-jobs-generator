992d9972069732b836ddd6a7511b855e4d005bde
anspfeifer <30416051+anspfeifer@users.noreply.github.com>
2017-08-19 00:37:10 -0300

    Update serverJobs.groovy

--- jobs/serverJobs.groovy
+++ jobs/serverJobs.groovy
@@ -1,15 +1,8 @@
 import static Utils.*
 
 jobFactory.freeStyleJob('shell-script-job') {
-    description getJobDescription()
+    
     logRotator (-1, 10)
     jdk ('Java 8')
-
-    parameters{
-        stringParam('BRANCH', '', "Branch do qual sera obtido o post script")
-    }
-    scm {
-        git('repo', 'branch')
-    }
 
 }
