32123c17ffd5d11761f8f9920299d7da1aaf08b8
Anselmo Pfeifer <anspfeifer@gmail.com>
2017-08-24 22:33:30 -0300

    WIP

--- jobs/generatorJobs.groovy
+++ jobs/generatorJobs.groovy
@@ -13,7 +13,7 @@ job('generator-jobs') {
     steps {
         sleep(100)
         dsl {
-            removeAction('DELETE')
+            removeAction('DISABLE')
             external 'jobs/**/*Jobs.groovy'
             additionalClasspath 'src/main/groovy'
             }
